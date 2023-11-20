dd26121868b7ed92196dc24b38172ee8402616b1
Mikhail Afonin <futurer@inbox.ru>
2023-11-21 00:10:27 +0300

    Spring Boot REST. Data JPA + Boot Starter Web

---
+++ src/test/java/com/mikhail/spring/springboot/SpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikhail.spring.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
